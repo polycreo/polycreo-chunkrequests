ws2ten1/ws2ten1-chunks

add initial resource

src/main/java/org/ws2ten1/chunks/ChunkRequest.java
@@ -1,0 +1,107 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ws2ten1.chunks;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import org.springframework.data.domain.Sort.Direction;
+
+/**
+ * Basic Java Bean implementation of {@code Chunkable}.
+ *
+ * @since 0.11
+ * @author daisuke
+ */
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChunkRequest implements Chunkable {
+	
+	@Getter
+	private String paginationToken;
+	
+	@Getter
+	private PaginationRelation paginationRelation;
+	
+	@Getter
+	private Integer maxPageSize;
+	
+	@Getter
+	private Direction direction;
+	
+	
+	/**
+	 * インスタンスを生成する。
+	 *
+	 * @param paginationToken the token
+	 */
+	public ChunkRequest(String paginationToken) {
+		this(paginationToken, PaginationRelation.NEXT, null, Direction.ASC);
+	}
+	
+	/**
+	 * インスタンスを生成する。
+	 *
+	 * @param maxPageSize max size
+	 */
+	public ChunkRequest(Integer maxPageSize) {
+		this(null, null, maxPageSize, Direction.ASC);
+	}
+	
+	/**
+	 * インスタンスを生成する。
+	 *
+	 * @param paginationToken the token
+	 * @param maxPageSize max size
+	 */
+	public ChunkRequest(String paginationToken, Integer maxPageSize) {
+		this(paginationToken, PaginationRelation.NEXT, maxPageSize, Direction.ASC);
+	}
+	
+	/**
+	 * インスタンスを生成する。
+	 *
+	 * @param direction the sort direction
+	 */
+	public ChunkRequest(Direction direction) {
+		this(null, null, null, direction);
+	}
+	
+	/**
+	 * インスタンスを生成する。
+	 *
+	 * @param paginationToken the token
+	 * @param direction the sort direction
+	 */
+	public ChunkRequest(String paginationToken, Direction direction) {
+		this(paginationToken, PaginationRelation.NEXT, null, direction);
+	}
+	
+	/**
+	 * インスタンスを生成する。
+	 *
+	 * @param maxPageSize max size
+	 * @param direction the sort direction
+	 */
+	public ChunkRequest(Integer maxPageSize, Direction direction) {
+		this(null, null, maxPageSize, direction);
+	}
+}

src/main/java/org/ws2ten1/chunks/Chunkable.java
@@ -1,0 +1,58 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ws2ten1.chunks;
+
+import org.springframework.data.domain.Sort.Direction;
+
+/**
+ * Abstract interface for value-based pagination information.
+ */
+public interface Chunkable {
+	
+	/**
+	 * Returns token for pagination.
+	 *
+	 * @return the token
+	 */
+	String getPaginationToken();
+	
+	/**
+	 * Returns the relation of current chunk to retrieve.
+	 *
+	 * @return the relation
+	 */
+	PaginationRelation getPaginationRelation();
+	
+	/**
+	 * Returns the number of items to be returned.
+	 *
+	 * @return the number of items of that chunk
+	 */
+	Integer getMaxPageSize();
+	
+	/**
+	 * Returns the direction the items sorted.
+	 *
+	 * @return the direction
+	 */
+	Direction getDirection();
+	
+	
+	enum PaginationRelation {
+		NEXT,
+		PREV
+	}
+}

src/main/java/org/ws2ten1/chunks/SimplePaginationTokenEncoder.java
@@ -1,0 +1,113 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ws2ten1.chunks;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Simple JSON and Base64 URL implementation for {@link PaginationTokenEncoder}.
+ *
+ * @since 0.24
+ * @version $Id$
+ * @author daisuke
+ */
+@Slf4j
+public class SimplePaginationTokenEncoder implements PaginationTokenEncoder {
+	
+	private static final String FIRST_KEY = "first_key";
+	
+	private static final String LAST_KEY = "last_key";
+	
+	@Getter
+	@Setter
+	@NonNull
+	private ObjectMapper objectMapper = new ObjectMapper();
+	
+	@Getter
+	@Setter
+	@NonNull
+	private Encoder encoder = Base64.getUrlEncoder().withoutPadding();
+	
+	@Getter
+	@Setter
+	@NonNull
+	private Decoder decoder = Base64.getUrlDecoder();
+	
+	
+	@Override
+	public String encode(Object firstKey, Object lastKey) {
+		Map<String, Object> map = new LinkedHashMap<>(2);
+		map.put(FIRST_KEY, firstKey);
+		map.put(LAST_KEY, lastKey);
+		try {
+			String json = objectMapper.writeValueAsString(map);
+			return encoder.encodeToString(json.getBytes(StandardCharsets.UTF_8));
+		} catch (JsonProcessingException e) {
+			throw new InvalidKeyExpressionException(e);
+		}
+	}
+	
+	@Override
+	public <T> Optional<T> extractFirstKey(String paginationToken, Class<T> clazz) {
+		return extractKey(paginationToken, FIRST_KEY, clazz);
+	}
+	
+	@Override
+	public Optional<String> extractFirstKey(String paginationToken) {
+		return extractKey(paginationToken, FIRST_KEY, String.class);
+	}
+	
+	@Override
+	public <T> Optional<T> extractLastKey(String paginationToken, Class<T> clazz) {
+		return extractKey(paginationToken, LAST_KEY, clazz);
+	}
+	
+	@Override
+	public Optional<String> extractLastKey(String paginationToken) {
+		return extractKey(paginationToken, LAST_KEY, String.class);
+	}
+	
+	<T> Optional<T> extractKey(String paginationToken, String key, Class<T> clazz) {
+		if (paginationToken == null) {
+			return Optional.empty();
+		}
+		try {
+			byte[] json = decoder.decode(paginationToken);
+			JsonNode keyNode = objectMapper.readTree(json).path(key);
+			T object = objectMapper.treeToValue(keyNode, clazz);
+			return Optional.ofNullable(object);
+		} catch (IOException | IllegalArgumentException e) {
+			log.warn("Invalid pagination token: {}", paginationToken);
+		}
+		return Optional.empty();
+	}
+}

src/test/java/org/ws2ten1/chunks/SimplePaginationTokenEncoderTest.java
@@ -1,0 +1,103 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ws2ten1.chunks;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Optional;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Test for {@link SimplePaginationTokenEncoder}.
+ */
+@Slf4j
+@SuppressWarnings("javadoc")
+public class SimplePaginationTokenEncoderTest {
+	
+	SimplePaginationTokenEncoder sut = new SimplePaginationTokenEncoder();
+	
+	
+	@Test
+	public void testString() {
+		String encoded = sut.encode("abc", "def");
+		log.info(encoded);
+		String first = sut.extractFirstKey(encoded).get();
+		String last = sut.extractLastKey(encoded).get();
+		assertThat(first, is("abc"));
+		assertThat(last, is("def"));
+	}
+	
+	@Test
+	public void testInteger() {
+		String encoded = sut.encode(123, 234);
+		log.info(encoded);
+		String first = sut.extractFirstKey(encoded).get();
+		String last = sut.extractLastKey(encoded).get();
+		assertThat(first, is("123"));
+		assertThat(last, is("234"));
+	}
+	
+	@Test
+	public void testDecodeFirstInvalid() {
+		Optional<String> actual = sut.extractFirstKey("1");
+		assertThat(actual.isPresent(), is(false));
+	}
+	
+	@Test
+	public void testDecodeLastInvalid() {
+		Optional<String> actual = sut.extractLastKey("1");
+		assertThat(actual.isPresent(), is(false));
+	}
+	
+	@Test
+	public void testCompositeKey() {
+		// setup
+		CompositeKey first = new CompositeKey("a", 123L);
+		CompositeKey last = new CompositeKey("z", 789L);
+		// exercise
+		String encoded = sut.encode(first, last);
+		log.info(encoded);
+		CompositeKey actualFirst = sut.extractFirstKey(encoded, CompositeKey.class).get();
+		CompositeKey actualLast = sut.extractLastKey(encoded, CompositeKey.class).get();
+		// verify
+		assertThat(actualFirst, is(first));
+		assertThat(actualLast, is(last));
+	}
+	
+	
+	@Data
+	private static class CompositeKey {
+		
+		private final String key1;
+		
+		private final long key2;
+		
+		
+		@JsonCreator
+		CompositeKey(@JsonProperty("key1") String key1, @JsonProperty("key2") long key2) {
+			this.key1 = key1;
+			this.key2 = key2;
+		}
+	}
+}

polycreo/polycreo-chunkrequests

src/main/java/org/ws2ten1/chunkrequests/ChunkRequest.java
@@ -1,0 +1,102 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ws2ten1.chunkrequests;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Basic Java Bean implementation of {@link Chunkable}.
+ */
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChunkRequest implements Chunkable {
+	
+	@Getter
+	private String paginationToken;
+	
+	@Getter
+	private PaginationRelation paginationRelation;
+	
+	@Getter
+	private Integer maxPageSize;
+	
+	@Getter
+	private Direction direction;
+	
+	
+	/**
+	 * Create instance.
+	 *
+	 * @param paginationToken the token
+	 */
+	public ChunkRequest(String paginationToken) {
+		this(paginationToken, PaginationRelation.NEXT, null, Direction.ASC);
+	}
+	
+	/**
+	 * Create instance.
+	 *
+	 * @param maxPageSize max size
+	 */
+	public ChunkRequest(Integer maxPageSize) {
+		this(null, null, maxPageSize, Direction.ASC);
+	}
+	
+	/**
+	 * Create instance.
+	 *
+	 * @param paginationToken the token
+	 * @param maxPageSize max size
+	 */
+	public ChunkRequest(String paginationToken, Integer maxPageSize) {
+		this(paginationToken, PaginationRelation.NEXT, maxPageSize, Direction.ASC);
+	}
+	
+	/**
+	 * Create instance.
+	 *
+	 * @param direction the sort direction
+	 */
+	public ChunkRequest(Direction direction) {
+		this(null, null, null, direction);
+	}
+	
+	/**
+	 * Create instance.
+	 *
+	 * @param paginationToken the token
+	 * @param direction the sort direction
+	 */
+	public ChunkRequest(String paginationToken, Direction direction) {
+		this(paginationToken, PaginationRelation.NEXT, null, direction);
+	}
+	
+	/**
+	 * Create instance.
+	 *
+	 * @param maxPageSize max size
+	 * @param direction the sort direction
+	 */
+	public ChunkRequest(Integer maxPageSize, Direction direction) {
+		this(null, null, maxPageSize, direction);
+	}
+}

src/main/java/org/ws2ten1/chunkrequests/Chunkable.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ws2ten1.chunkrequests;
+
+/**
+ * Abstract interface for value-based pagination information.
+ */
+public interface Chunkable {
+	
+	/**
+	 * Returns token for pagination.
+	 *
+	 * @return the token
+	 */
+	String getPaginationToken();
+	
+	/**
+	 * Returns the relation of current chunk to retrieve.
+	 *
+	 * @return the relation
+	 */
+	PaginationRelation getPaginationRelation();
+	
+	/**
+	 * Returns the number of items to be returned.
+	 *
+	 * @return the number of items of that chunk
+	 */
+	Integer getMaxPageSize();
+	
+	/**
+	 * Returns the direction the items sorted.
+	 *
+	 * @return the direction
+	 */
+	Direction getDirection();
+	
+	
+	enum PaginationRelation {
+		NEXT,
+		PREV
+	}
+}

src/main/java/org/ws2ten1/chunkrequests/SimplePaginationTokenEncoder.java
@@ -1,0 +1,109 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ws2ten1.chunkrequests;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Simple JSON and Base64 URL implementation for {@link PaginationTokenEncoder}.
+ */
+@Slf4j
+public class SimplePaginationTokenEncoder implements PaginationTokenEncoder {
+	
+	private static final String FIRST_KEY = "first_key";
+	
+	private static final String LAST_KEY = "last_key";
+	
+	@Getter
+	@Setter
+	@NonNull
+	private ObjectMapper objectMapper = new ObjectMapper();
+	
+	@Getter
+	@Setter
+	@NonNull
+	private Encoder encoder = Base64.getUrlEncoder().withoutPadding();
+	
+	@Getter
+	@Setter
+	@NonNull
+	private Decoder decoder = Base64.getUrlDecoder();
+	
+	
+	@Override
+	public String encode(Object firstKey, Object lastKey) {
+		Map<String, Object> map = new LinkedHashMap<>(2);
+		map.put(FIRST_KEY, firstKey);
+		map.put(LAST_KEY, lastKey);
+		try {
+			String json = objectMapper.writeValueAsString(map);
+			return encoder.encodeToString(json.getBytes(StandardCharsets.UTF_8));
+		} catch (JsonProcessingException e) {
+			throw new InvalidKeyExpressionException(e);
+		}
+	}
+	
+	@Override
+	public <T> Optional<T> extractFirstKey(String paginationToken, Class<T> clazz) {
+		return extractKey(paginationToken, FIRST_KEY, clazz);
+	}
+	
+	@Override
+	public Optional<String> extractFirstKey(String paginationToken) {
+		return extractKey(paginationToken, FIRST_KEY, String.class);
+	}
+	
+	@Override
+	public <T> Optional<T> extractLastKey(String paginationToken, Class<T> clazz) {
+		return extractKey(paginationToken, LAST_KEY, clazz);
+	}
+	
+	@Override
+	public Optional<String> extractLastKey(String paginationToken) {
+		return extractKey(paginationToken, LAST_KEY, String.class);
+	}
+	
+	<T> Optional<T> extractKey(String paginationToken, String key, Class<T> clazz) {
+		if (paginationToken == null) {
+			return Optional.empty();
+		}
+		try {
+			byte[] json = decoder.decode(paginationToken);
+			JsonNode keyNode = objectMapper.readTree(json).path(key);
+			T object = objectMapper.treeToValue(keyNode, clazz);
+			return Optional.ofNullable(object);
+		} catch (IOException | IllegalArgumentException e) {
+			log.warn("Invalid pagination token: {}", paginationToken);
+		}
+		return Optional.empty();
+	}
+}

src/test/java/org/ws2ten1/chunkrequests/SimplePaginationTokenEncoderTest.java
@@ -1,0 +1,103 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ws2ten1.chunkrequests;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Optional;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Test for {@link SimplePaginationTokenEncoder}.
+ */
+@Slf4j
+@SuppressWarnings("javadoc")
+public class SimplePaginationTokenEncoderTest {
+	
+	SimplePaginationTokenEncoder sut = new SimplePaginationTokenEncoder();
+	
+	
+	@Test
+	public void testString() {
+		String encoded = sut.encode("abc", "def");
+		log.info(encoded);
+		String first = sut.extractFirstKey(encoded).get();
+		String last = sut.extractLastKey(encoded).get();
+		assertThat(first, is("abc"));
+		assertThat(last, is("def"));
+	}
+	
+	@Test
+	public void testInteger() {
+		String encoded = sut.encode(123, 234);
+		log.info(encoded);
+		String first = sut.extractFirstKey(encoded).get();
+		String last = sut.extractLastKey(encoded).get();
+		assertThat(first, is("123"));
+		assertThat(last, is("234"));
+	}
+	
+	@Test
+	public void testDecodeFirstInvalid() {
+		Optional<String> actual = sut.extractFirstKey("1");
+		assertThat(actual.isPresent(), is(false));
+	}
+	
+	@Test
+	public void testDecodeLastInvalid() {
+		Optional<String> actual = sut.extractLastKey("1");
+		assertThat(actual.isPresent(), is(false));
+	}
+	
+	@Test
+	public void testCompositeKey() {
+		// setup
+		CompositeKey first = new CompositeKey("a", 123L);
+		CompositeKey last = new CompositeKey("z", 789L);
+		// exercise
+		String encoded = sut.encode(first, last);
+		log.info(encoded);
+		CompositeKey actualFirst = sut.extractFirstKey(encoded, CompositeKey.class).get();
+		CompositeKey actualLast = sut.extractLastKey(encoded, CompositeKey.class).get();
+		// verify
+		assertThat(actualFirst, is(first));
+		assertThat(actualLast, is(last));
+	}
+	
+	
+	@Data
+	private static class CompositeKey {
+		
+		private final String key1;
+		
+		private final long key2;
+		
+		
+		@JsonCreator
+		CompositeKey(@JsonProperty("key1") String key1, @JsonProperty("key2") long key2) {
+			this.key1 = key1;
+			this.key2 = key2;
+		}
+	}
+}